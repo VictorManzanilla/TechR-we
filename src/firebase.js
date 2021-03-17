962d6ba2052c0d6dc3705844ddde142cf745bc1b
Victor <vmanzanilla7@gmail.com>
2021-03-16 19:15:38 -0500

    register form made, working on auth

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase" 
+ 
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+    apiKey: "AIzaSyAbsdo06pT25hxwqQDhQkZYFdlSp8MttBk",
+    authDomain: "techr-we.firebaseapp.com",
+    projectId: "techr-we",
+    storageBucket: "techr-we.appspot.com",
+    messagingSenderId: "529440984322",
+    appId: "1:529440984322:web:90aa4f9152e74b156bc61b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export const auth = firebase.auth()
+  export const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
